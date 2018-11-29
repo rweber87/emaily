3763ed50eb9443dadfc006a11fec4c4d41dbc232
Rob Weber <rob.weber87@gmail.com>
2018-11-28 22:27:01 -0500

    committing updates to package specs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app =  express();
 
-
 app.get('/', (req, res) => {
 	res.send({hi: 'there'});
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
