aca848528a1783a6ccfe093a6acedcd64115742f
Rob Weber <rob.weber87@gmail.com>
2018-11-29 00:08:58 -0500

    finished google oauth process

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
-		}, accessToken => {
-			console.log(accessToken);
+		}, (accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile', profile);
 		}
 	)
 );
@@ -23,7 +25,7 @@ app.get(
 	})
 );
 
-app.get('/'
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
